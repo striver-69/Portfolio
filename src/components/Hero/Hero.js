f65209157d0af4ea075b3fb7ae3369145147c028
Chiranjivee <chiranjivee960@gmail.com>
2021-10-14 00:01:12 +0530

    Modified styled component for about me

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        My main goal is to be a creative full stack engineer
+      </SectionText>
+      <Button onClick={() => (window.location = 'https://google.com')}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
